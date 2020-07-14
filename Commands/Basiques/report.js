8976f48166d737c2d7dbb09c513bd7ed367609f5
WeeskyBDW <bibou.gamers@gmail.com>
2020-07-15 01:38:19 +0200

    :art: Clean the report command to improve readability and fix a NPE error

--- Commands/Basiques/report.js
+++ Commands/Basiques/report.js
@@ -2,18 +2,17 @@ const config = require("entasia/config")
 const reportReact = require("../../Features/poukave")
 const { MessageEmbed } = require("discord.js")
 async function askInformation(message, askMessage = "Ce message ne devrait pas apparaitre, merci de contacter iTrooz_#2050 ou WeeskyBDW#6172", time = 30000, filter = (user => message.author.bot == false)) {
-    if(message === undefined) return console.log("[Erreur] Commande de report.js : le message n'a pas été spécifié dans la fonction ")
+    if(!message) return console.log("[Erreur] Commande de report.js : le message n'a pas été spécifié dans la fonction ")
     return new Promise(async(resolve, reject) => {
         message.author.send(askMessage).then(async questionMsg => {
 
-            const responce = await questionMsg.channel.awaitMessages(filter, {max: 1, time: time, errors: ['time']}).catch(async () =>  {
+            const responce = await questionMsg.channel.awaitMessages(filter, {max: 1, time: time, errors: ['time']}).catch(async () => {
                 reject(`Tu as mis trop de temps à faire ton signalement, merci de refaire .report dans ${config.channels.salon_bot}`)
-                return
             })
-            if(responce == undefined) return
+            if(!responce) return
             const msg = responce.array()[0]
+            if(!msg) reject(`Tu as mis trop de temps à faire ton signalement, merci de refaire .report dans ${config.channels.salon_bot}`)
             if(msg.author.bot) return
-            if(msg == undefined) reject(`Tu as mis trop de temps à faire ton signalement, merci de refaire .report dans ${config.channels.salon_bot}`)
             await resolve({questionMsg, msg})
         }).catch(() => {return message.reply(`Je ne peux pas t'envoyer de messages privés, merci de vérifier que tu acceptes les messages privés venant de ce serveur`)})
     })
@@ -28,10 +27,13 @@ class report {
         }
         catch(e) {}
 
-        let reportedPlayer = await askInformation(message, "Quel est le pseudo de la personne que tu veux signaler ? :smile:").catch(e => {return message.channel.send(e)})
-        if(reportedPlayer == undefined || reportedPlayer.msg == undefined) return
+        let reportedPlayer = await askInformation(message, "Quel est le pseudo de la personne que tu veux signaler ? :smile:").catch(e =>
+        {
+            return message.channel.send(e)
+        })
+        if(!reportedPlayer|| !reportedPlayer.msg) return
         const reportReason = await askInformation(message, "Quelle est la raison du signalement ? :smiley:").catch(e => {return message.channel.send(e)})
-        if(reportReason == undefined || reportReason.msg == undefined) return
+        if(!reportReason|| !reportReason.msg) return
         //console.log("debug 1 : " + reportedPlayer.msg.constructor + "\n" + "debug 2" + reportReason.msg)
         const embed = new MessageEmbed()
             .setAuthor("Report de " + message.author.username, message.author.avatarURL(), "http://"+message.author.id+".fr")
