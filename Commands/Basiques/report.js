0db2ba36e02a9a2fb518b3bad4fbed464321cd25
WeeskyBDW <bdwadv@gmail.com>
2020-08-19 17:00:24 +0200

    :sparkles: Support of attachment in report reason
    and some bugs fix

--- Commands/Basiques/report.js
+++ Commands/Basiques/report.js
@@ -7,22 +7,27 @@ class report {
         
     }
     async execute(message) {
-        message.channel.send("Regarde tes messages privés :smile:")
-        message.author.send("Hey ! Je vais t'aider dans ta démarches de report").catch(no)
 
+        message.author.send("Hey ! Je vais t'aider dans ta démarches de report")
+            .then(() => {
+                message.channel.send("Regarde tes messages privés")
+            })
+            .catch(() => {
+                return message.channel.send("Je ne peux pas t'envoyer de messages privés")
+            })
         let reportedPlayer = await askInformation(message, "Quel est le pseudo de la personne que tu veux signaler ? :smile:")
             .catch(e => {return message.channel.send(e)})
-        if(reportedPlayer == undefined || reportedPlayer.msg == undefined) return
         const reportReason = await askInformation(message, "Quelle est la raison du signalement ? :smiley: (tu peux ajouter un screen pour appuyer ton signalement)")
             .catch(e => {return message.channel.send(e)})
-        if(reportReason == undefined || reportReason.msg == undefined) return
+        if(reportReason || reportReason.msg) return
+        const reason = reportReason.msg.content ? reportReason.msg.content : "Aucune raison spécifié"
         const embed = new MessageEmbed()
             .setAuthor("Report de " + message.author.username, message.author.avatarURL(), "http://"+message.author.id+".fr")
             .addField("Envers", reportedPlayer.msg.content)
-            .addField("Raison", reportReason.msg.content)
+            .addField("Raison", reason)
 
-        if (reportReason.msg.attachments[0]) embed.addField("Fichier intégré au signalement", reportReason.msg.attachments.first().url)
-        config.channels.reportcheck.send(embed).then(m => reportReact(m, reportedPlayer.msg.content, reportReason.msg.content))
+        if (reportReason.msg.attachments.first()) embed.addField("Fichier intégré au signalement", reportReason.msg.attachments.first().url)
+        config.channels.reportcheck.send(embed).then(m => reportReact(m, reportedPlayer.msg.content, reason, reportReason.msg.attachments.first()))
         message.author.send("Ton report a bien été pris en compte, Merci")
     }
 
