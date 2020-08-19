2b661ade9accd6cdbe31fa784c96573d45c2cd42
WeeskyBDW <bdwadv@gmail.com>
2020-08-19 17:09:02 +0200

    :recycle: Fix remove of tabulations for basic.js file in last commit

--- Commands/Basiques/basic.js
+++ Commands/Basiques/basic.js
@@ -1,33 +1,33 @@
 const { secondsToTime } = require('entasia/GlobalAPI').functions
 
 class uptime{
-    constructor(){
-    }
+	constructor(){
+	}
     execute(message){
-        message.channel.send("Je vous surveille depuis : "+secondsToTime(bot.uptime/1000))
-    }
+		message.channel.send("Je vous surveille depuis : "+secondsToTime(bot.uptime/1000))
+	}
 }
 class avatar{
-    constructor(){
-    }
+	constructor(){
+	}
     execute(message, arg){
-        let ava
-        if(arg[1]){
-            if (message.mentions.users.size > 0)ava = message.mentions.members.first()
-            else{
-                ava = message.guild.members.cache.get(arg[1])
-                if(!ava)return message.channel.send("Cet utilisateur n'existe pas !")
-            }
-        }else ava = message.member
-        let url = ava.user.displayAvatarURL({format: "png", dynamic: true, size: 4096})
-        if(!url)return message.channel.send("Cet utilisateur n'a pas d'avatar !")
-        let t = url.indexOf("?")
-        if (t != -1)url = url.substring(0, t)
-        url += "?size=1024"
-        let embed = new CustomEmbed([message.author.username, "avatar"]).setHeader("Avatar de "+ava.user.username)
-        embed.setImage(url)
-        message.channel.send(embed)
-    }
+		let ava
+		if(arg[1]){
+			if (message.mentions.users.size > 0)ava = message.mentions.members.first()
+			else{
+				ava = message.guild.members.cache.get(arg[1])
+				if(!ava)return message.channel.send("Cet utilisateur n'existe pas !")
+			}
+		}else ava = message.member
+		let url = ava.user.displayAvatarURL({format: "png", dynamic: true, size: 4096})
+		if(!url)return message.channel.send("Cet utilisateur n'a pas d'avatar !")
+		let t = url.indexOf("?")
+		if (t != -1)url = url.substring(0, t)
+		url += "?size=1024"
+		let embed = new CustomEmbed([message.author.username, "avatar"]).setHeader("Avatar de "+ava.user.username)
+		embed.setImage(url)
+		message.channel.send(embed)
+	}
 }
 
 module.exports.commands = { uptime, avatar }
