3ff10c9f655d199a3dff1673ff8b8b3cdb7b0b82
itrooz <team.mortelgames@gmail.com>
2020-09-11 18:11:40 +0200

    en pause

--- Commands/Basiques/time.js
+++ Commands/Basiques/time.js
@@ -8,7 +8,7 @@ class time{
 		let target
 		if(arg[1]){
 			if (message.mentions.users.size > 0)target = message.mentions.members.first()
-			else target = message.guild.members.get(arg[1])
+			else target = message.guild.members.cache.get(arg[1])
 			if(!target)return message.channel.send("Cet utilisateur n'existe pas !")
 		}else target = message.member
 		message.channel.send(target.user.username+" est la depuis "+secondsToTime((Date.now()-target.joinedTimestamp)/1000))
